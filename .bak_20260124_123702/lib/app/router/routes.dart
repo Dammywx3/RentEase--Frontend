626dab0a8fd8c9129852f6a77da84347ee353373
Adebbayo Damilare <damilareomoadebayo@gmail.com>
2026-01-30 10:27:14 +0100

    chore: stop tracking backups/build outputs

--- .bak_20260124_123702/lib/app/router/routes.dart
+++
@@ -1,8 +0,0 @@
-class AppRoutes {
-  static const login = '/login';
-  static const register = '/register';
-
-  // Shell entry routes
-  static const tenantShell = '/tenant';
-  static const agentShell  = '/agent';
-}
--- .bak_fix_all_20260125_181807/lib/core/theme/app_radii.dart
+++
@@ -1,8 +0,0 @@
-class AppRadii {
-  static const sm = 10.0;
-  static const md = 14.0;
-  static const lg = 18.0;
-  static const xl = 22.0;
-
-  static const pill = 999.0;
-}
